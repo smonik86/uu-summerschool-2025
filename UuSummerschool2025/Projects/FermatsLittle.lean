75d7f40b2faf5dfa6e9b2e340927b895b8ac59ff
smonik86 <monica070721@gmail.com>
2025-07-24 23:50:00 +0200

    foo

--- UuSummerschool2025/Projects/FermatsLittle.lean
+++ UuSummerschool2025/Projects/FermatsLittle.lean
@@ -1,11 +1,33 @@
 import Mathlib
+open Finset
 def B (n : ℕ) (x y : ℕ) := (x + y)^n
-theorem binomial_theorem_nat (x y : ℕ) :  ∀ n : ℕ, (x + y)^n = ∑ k in range (n + 1), Nat.choose n k * x^k * y^(n - k)
-  | zero =
-    -- base case: n = 0
-  simp only [pow_zero, range_one, Nat.choose_zero_right, pow_zero, mul_one, mul_one, sum_singleton]
-  | succ d hd =>
-    -- inductive step: assume true for d, prove for d + 1
-    rw [pow_succ, hd]
-    rw [mul_sum]
-    rw [← sum_range_succ']
+theorem binomial_theorem_nat (x y : ℕ) :  ∀ n : ℕ, (x + y)^n = ∑ k ∈ Ico 0 (n + 1), Nat.choose n k * x^k * y^(n - k) := by
+  intro n
+  induction n
+  · simp
+  next k ih =>
+    simp
+    rw[pow_add]
+    simp
+    rw[ih]
+    rw[Finset.sum_mul]
+    simp_rw[Nat.mul_add]
+    rw[Finset.sum_add_distrib]
+    simp
+    rw[Finset.sum_range_succ]
+    simp
+    rw[←pow_succ]
+    nth_rewrite 3[←pow_one y]
+    -- rw[←pow_succ] -- not sure why this isn't working for y?
+    rw [←Nat.choose_succ_right] -- pascal's identity
+    nth_rewrite 4[Nat.choose_succ_succ]
+
+
+
+
+
+
+
+
+
+    sorry
